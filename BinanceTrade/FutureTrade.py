4de87e2e7bbfdb438b66908c0d20d804b24fb880
Tanapon <tuntivigrom@gmail.com>
2021-08-21 18:50:13 +0700

    "fix stop loss"

--- BinanceTrade/FutureTrade.py
+++ BinanceTrade/FutureTrade.py
@@ -30,7 +30,7 @@ def getAssetUSDT():
     return int(result[1].balance)
 
 #< แก้ไข เพิ่ม function 6-27-2021
-def PlaceOrderAtMarket(position,symbol,amount,lev,act_price_percent=0.1,cb=1,stoploss_Percent = 1):
+def PlaceOrderAtMarket(position,symbol,amount,lev,act_price_percent=0.1,cb=1,stoploss_Percent = 5):
     """
     UPDATE LOGIC 6-27-2021 (ดูวิดิโอในกลุ่ม)
     position : Long or Short

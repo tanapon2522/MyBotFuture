f683c90b6e26169c2fe48df3475f40fcae6223e8
Tanapon <tuntivigrom@gmail.com>
2021-09-19 22:03:20 +0700

    add

--- BinanceTrade/FutureTrade.py
+++ BinanceTrade/FutureTrade.py
@@ -30,7 +30,7 @@ def getAssetUSDT():
     return int(result[1].balance)
 
 #< แก้ไข เพิ่ม function 6-27-2021
-def PlaceOrderAtMarket(position,symbol,amount,lev,act_price_percent=0.01,cb=0.7,stoploss_Percent =5):
+def PlaceOrderAtMarket(position,symbol,amount,lev,act_price_percent=0.01,cb=1,stoploss_Percent =5):
     """
     UPDATE LOGIC 6-27-2021 (ดูวิดิโอในกลุ่ม)
     position : Long or Short
